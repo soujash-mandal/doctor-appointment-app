2c626c436c5ab8731654a1b7332c2526adbbeca1
Soujash Mandal <2002soujash@gmail.com>
2023-09-21 20:44:36 +0530

    first commit

---
+++ client/src/pages/ProfilePage.js
@@ -0,0 +1,12 @@
+import Layout from "antd/lib/layout/layout";
+import React from "react";
+
+const ProfilePage = () => {
+  return (
+    <Layout>
+      <div>ProfilePage</div>{" "}
+    </Layout>
+  );
+};
+
+export default ProfilePage;
